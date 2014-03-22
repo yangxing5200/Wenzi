737312d4b423abba2d8e1ba99690185fb0fbfaa5
yangxing5200 <418505093@qq.com>
2014-03-23 04:08:25 +0800

    加入了MiniProfiler

--- WenziBlog/Protal/Global.asax.cs
+++ WenziBlog/Protal/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StackExchange.Profiling;
 
 namespace Protal
 {
@@ -23,6 +24,10 @@ namespace Protal
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+            MiniProfilerEF.Initialize();
         }
+        protected void Application_BeginRequest() { if (Request.IsLocal) { MiniProfiler.Start(); } }
+        protected void Application_EndRequest() { MiniProfiler.Stop(); }
     }
+
 }
---
+++ WenziBlog/Protal/Models/ExtNetModel.cs
@@ -0,0 +1,8 @@
+namespace Protal.Models
+{
+    public class ExtNetModel
+    {
+        public string Title { get; set; }
+        public string TextAreaEmptyText { get; set; }
+    }
+}
