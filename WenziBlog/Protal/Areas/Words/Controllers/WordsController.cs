22bde8e726d4313f35fafe875c666b447f5dda9b
yangxing5200 <418505093@qq.com>
2014-03-22 14:51:08 +0800

    链接Mysql并成功显示数据

---
+++ WenziBlog/Protal/Areas/Words/Controllers/WordsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Protal.Models;
+
+namespace Protal.Areas.Words.Controllers
+{
+    public class WordsController : Controller
+    {
+        blogdbEntities1 db=new blogdbEntities1();
+        //
+        // GET: /Words/Words/
+
+        public ActionResult Index()
+        {
+            return View(db.wz_word.ToList());
+        }
+
+    }
+}
---
+++ WenziBlog/Protal/Areas/Words/WordsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Protal.Areas.Words
+{
+    public class WordsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Words";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Words_default",
+                "Words/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
